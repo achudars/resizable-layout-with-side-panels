db5d5b0ecbe8a1ea6e892c51e9a7b1ed07f5a2c5
Aleksandrs Cudars <achudars@gmail.com>
2013-10-09 00:33:08 +0100

    Major layout set up. Left panle populated with tabs.

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(function() {
+  // resizable panels
+  var container = $('#outer-container').layout();
+  var middle = $('#middle-container').layout();
+  var top = $('#top-container').layout();
+  var bottom = $('#bottom-container').layout();
+
+  var halfOfMiddleHeight = ($('#middle-container').height() / 2);
+  
+
+  container.sizePane("west", 360);
+  container.sizePane("east", 360);
+
+
+  middle.sizePane("north", halfOfMiddleHeight);
+  
+  var halfOfTopWidth = ($('#top-container').width() / 2);
+
+  top.sizePane("east", halfOfTopWidth);
+  bottom.sizePane("east", halfOfTopWidth);
+
+
+  // tabs
+
+  $( "#tabs" ).tabs();
+});
